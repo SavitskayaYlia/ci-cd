fcd6f7fc7a96e0fbab28fcd28b801b700432dea9
SavitskayaYlia <Savitskaya.Ylia@gmail.com>
2024-08-21 11:37:16 +0300

    Update greeting.py

--- python/greeting.py
+++ python/greeting.py
@@ -5,3 +5,14 @@ class Greeting:
     
     def print_message(self):
         print(f"{self.greeting}, {self.name}!")
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 3:
+        print("Usage: python greet.py [name] [greeting]")
+        sys.exit(1)
+    name = sys.argv[1]
+    greeting = sys.argv[2]
+    greeting_instance = Greeting(name, greeting)
+    greeting_instance.print_message()
